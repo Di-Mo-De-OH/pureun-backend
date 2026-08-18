4418994ebf31298721e8a169af7cf49de80f65f1
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-08-18 22:38:49 +0900

    feat: 상품 전체조회 스키마 작성

--- app/products/schemas/read.py
+++ app/products/schemas/read.py
@@ -1,13 +1,11 @@
 from pydantic import BaseModel, ConfigDict, Field
 
-from app.products.models import Category
-
 
 class ProductSummaryResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
-    id: str = Field(examples=["DSGCXZVZX@2123..."])
+    id: str = Field(examples=["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."])
     name: str = Field(examples=["오징어"])
     price: int = Field(examples=[10000])
-    discount_price: int | None = Field(examples=[5000],default=None)
+    discount_price: int | None = Field(examples=[5000], default=None)
     thumbnail_image_key: str
