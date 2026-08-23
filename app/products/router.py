43acf3f77c56369290b5a2d379b84404a7d9565a
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-08-23 16:56:02 +0900

    feat: 상품 단 건조회 router 구현

--- app/products/router.py
+++ app/products/router.py
@@ -9,9 +9,9 @@ from app.core.s3 import PresignedUrlRequest, PresignedUrlResponse, create_presig
 from app.core.utils.pagination import CursorPage, CursorPageParams
 from app.products.models import Product
 from app.products.schemas.create import ProductCreateRequest, ProductCreateResponse
-from app.products.schemas.read import ProductSummaryResponse
+from app.products.schemas.read import ProductSummaryResponse,ProductDetailResponse
 from app.products.services.create import create_product
-from app.products.services.read import get_products
+from app.products.services.read import get_products,get_product_detail
 from app.products.utils.responses import PRODUCT_CREATE_RESPONSES
 
 router = APIRouter(prefix="/products", tags=["products"])
@@ -41,3 +41,10 @@ async def read_products_router(
     db: DbSession, params: Annotated[CursorPageParams, Depends()]
 ) -> CursorPage[ProductSummaryResponse]:
     return await get_products(db, params)
+
+@router.get("/{product_id}",status_code=status.HTTP_200_OK, response_model=ProductDetailResponse)
+async def read_product_router(
+        db: DbSession,
+        product_id :str
+)->Product:
+    return await get_product_detail(db,product_id)
