9751047e98849dd05753973c85eb06edbabc7e71
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-08-25 23:34:48 +0900

    docs: 코드 이해를 위한 주석 추가

--- app/products/services/update.py
+++ app/products/services/update.py
@@ -42,19 +42,31 @@ async def update_product(db: AsyncSession, product_id: str, request: ProductUpda
                 db.add(ProductOption(product_id=product.id, **option_data.model_dump(exclude={"id"})))
 
         request_image_ids = {img.id for img in request.images if img.id is not None}
+        # 요청 받은 데이터의 id 를 뽑는 과정
         for img in product.images:
+            # db값을 img 에 담는 for문
             if img.id not in request_image_ids:
+                # db에서 꺼내온 id 값이 요청으로 들어온 id 값에 없다면
                 await db.delete(img)
+                # 해당 로우 삭제
 
         existing_img_ids = {img.id: img for img in product.images}
+        # id 값을 키로 ProductImage 객체를 가져옴 ex) {"img_id":ProductImage객체}
         for img_data in request.images:
+            # 요청받은 데이터를 img_data에 담는 과정
             if img_data.id is not None and img_data.id in existing_img_ids:
+                # 해당 데이터의 id값이 있고 db에 존재한다면
                 existing_img = existing_img_ids[img_data.id]
+                # id로 딕셔너리에서 실제 ProductImage 객체를 꺼내옴
+                # (딕셔너리 아님,객체라서 .필드명으로 접근가능 즉 속성으로 접근가능)
                 existing_img.image_key = img_data.image_key
+                # 실제 데이터를 요청받은 데이터로 덮음
                 existing_img.sort_order = img_data.sort_order
+                # 실제 데이터를 요청받은 데이터로 덮음
 
             else:
                 db.add(ProductImage(product_id=product.id, **img_data.model_dump(exclude={"id"})))
+                # 만약 요청받은 id값이 db에 존재하지 않는다면 새로운 로우로 데이터를 만듦
 
         label_data = request.label.model_dump()
         for field, value in label_data.items():
